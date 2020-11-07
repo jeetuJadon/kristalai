f72a1ac01c570d9c9f403cd39fe89539ed2f6261
Jitendrasinh Jadon <jituj6@gmail.com>
2020-11-07 13:49:26 +0530

    Adding all related Files

---
+++ src/test/java/com/kristalai/kristalai/KristalaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristalai.kristalai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KristalaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
